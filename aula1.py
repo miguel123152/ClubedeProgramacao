ea39bc62c0177eb0d5acd71c5d3331a3e0a47e02
miguel123152 <miguel.abjaud@loyola.g12.br>
2025-08-13 14:54:01 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,14 @@
+nome= "Miguel"
+turma= "B"
+anos= '14 anos'
+
+print('oi, eu sou: ' +nome)
+print('tenho: '+anos)
+
+nota1=int (input ('digite a primeira nota: '))
+nota2=int (input ('digite a segunda nota: '))
+nota3=int (input ('digite a terceira nota: '))
+nota4=int (input ('digite a quarta nota: '))
+
+resultado=(nota1+nota2+nota3+nota4)/4
+print("A média é: "+str(resultado))
