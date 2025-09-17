d907ac109981de66dfc313137299dac5f42c51db
miguel123152 <miguel.abjaud@loyola.g12.br>
2025-09-17 14:54:02 -0300

    Create aula6_calculadora.py

---
+++ aula6_calculadora.py
@@ -0,0 +1,47 @@
+def soma(val1,val2):
+    resultado=val1+val2
+    print(resultado)
+
+def subtracao(val1,val2):
+    resultado=val1-val2
+    print(resultado)
+
+def multiplicacao(val1,val2):
+    resultado=val1*val2
+    print(resultado)
+
+def divisao(val1,val2):
+    resultado=val1/val2
+    print(resultado)
+
+
+
+while 1:
+    num1=int(input('Qual o primeiro número?'))
+
+    num2=int(input('Qual o segundo número?'))
+
+    sinal=input('Qual a operação?').lower()
+    if num2==0 and sinal==('divisao') or sinal==('/'):
+        print('Erro. Impossível dividir por zero.')
+        if (input('Usar novamente?').lower())==('sim').lower():
+            continue
+        else:
+            break
+    if sinal==("soma") or sinal==("+"):
+        soma(num1,num2)
+        
+    elif sinal==("subtracao") or sinal==("-"):
+        subtracao(num1,num2)
+        
+    elif sinal==('multiplicacao') or sinal==("*"):
+        multiplicacao(num1,num2)
+        
+    elif sinal==('divisao') or sinal==("/"):
+        divisao(num1,num2)
+  
+    else:  
+        print ('Inválido')
+    if (input('Usar novamente?').lower())=='nao':
+        break
+        
